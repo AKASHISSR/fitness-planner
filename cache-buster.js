78e4337a4c7553488ebb7c1641ca9e0c128de9c0
Никита Лаптинский <nikitalaptinskii@MacBook-Air-Nikita.local>
2025-06-05 19:59:25 +0300

    Updates

--- cache-buster.js
+++ cache-buster.js
@@ -2,7 +2,7 @@
 (function() {
   'use strict';
   
-  const CACHE_VERSION = '20241220-044';
+  const CACHE_VERSION = '20241220-045';
   const STORAGE_KEY = 'fitgenius_cache_version';
   
   // Проверяем версию кеша
